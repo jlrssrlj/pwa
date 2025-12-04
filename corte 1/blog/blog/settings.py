82186bacfeb31c7abde0c84f4cb7705aa2a5f20e
Jorge Ramos <ramos014@hotmai.com>
2025-12-03 20:02:14 -0500

    Agregando variables de entorno

--- corte 1/blog/blog/settings.py
+++ corte 1/blog/blog/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
+from decouple import config
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,9 +23,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-=cjd@obq&nqbb@cp5741*^3s)q*yu+rdttw%2g*o+u)t!lt9g('
+SECRET_KEY = config('SECRET_KEY')
+DEBUG = config('DEBUG', default=False, cast=bool)
 
 # SECURITY WARNING: don't run with debug turned on in production!
+
 DEBUG = True
 
 ALLOWED_HOSTS = [
